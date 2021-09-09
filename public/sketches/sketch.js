2d2eadc7ecd25319b87d94824e8c3e0d49720633
Michael Rubanov <michael@lusha.com>
2021-09-10 00:10:13 +0300

    Revert "test"
    This reverts commit 8fe4970995dfa8097fa435c6df69b08b7d5c3312.

--- public/sketches/sketch.js
+++ public/sketches/sketch.js
@@ -1,14 +1,14 @@
-/* eslint-disable no-undef */
 const D  = 400;
+const CS = 20;
+const C  = [D/2,D/2,CS];
+let PO = [C[0], C[1]];
 
-// eslint-disable-next-line no-unused-vars
 function setup() {
-    createCanvas(D,D);
-    background(1);
-    console.log('hlo from setup', {canvas: {D}});
+  createCanvas(D,D);
+  background(1);
+  console.log('hlo from setup',{canvas: {D}});
 }
 
-// eslint-disable-next-line no-unused-vars
 function draw() {
 
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 const static = require('node-static');
 const http = require('http');
 const path = require('path');
@@ -5,68 +7,41 @@ const path = require('path');
 // TODO handle hot reload
 
 /**
- * Custom InvalidArgumentException error type
- * @date 2021-09-09
- * @param {any} message
- * @returns {any}
- */
-class InvalidArgumentException extends Error {
-    constructor(message) {
-        super(message); // (1)
-        this.name = 'InvalidArgumentException';
-    }
-}
-
-/**
- * Return default static content local fs path
+ * return default static content local fs path
  * @date 2021-09-09
  * @throws {InvalidArgumentException}
  * @returns {string}
  */
 function publicContentSource() {
-    let publicPath = path.join(__dirname, '../public');
+  let publicPath = path.join(__dirname, '../public');
 
-    try {
-        // A TypeError is thrown if any of the path segments is not a string.
-        publicPath = path.join(__dirname, '../public'); 
-        return publicPath;
-    } catch (error) {
-        // folowing clause is for node v14 error handling
-        if (error.code === 'ERR_INVALID_ARG_TYPE') {
-            const message =`Directory ${__dirname }../public` + ' not found.';
-            throw new InvalidArgumentException(message);
-        } else {
-            throw error;
-        }
+  try {
+     // A TypeError is thrown if any of the path segments is not a string.
+     publicPath = path.join(__dirname, '../public'); 
+     return publicPath;
+  } catch (error) {
+    // folowing clause is for node v14
+    if (error.code === 'ERR_INVALID_ARG_TYPE') {
+      throw new Error('Directory ' + __dirname + '../public' + ' not found,');
+    } else {
+      throw error;
     }
+  }
 }
 
-//TODO refactor to di
-//TODO jsdocs
 const SERVER_ROOT = new(static.Server)(publicContentSource());
-function staticContentRequestListener(staticContentRoot = SERVER_ROOT) {
-    return (req, res) => {
-        staticContentRoot.serve(req, res);
-    };
+
+const options = {};
+
+function requestListener(staticContentRoot = SERVER_ROOT) {
+  return (req, res) => {
+    staticContentRoot.serve(req, res);
+  };
 }
 
-//TODO jsdocs
-function startHttpd(options = {}, 
-    requestListener = staticContentRequestListener(), 
-    onListen) {
-    const server = http.createServer(options, requestListener);
+const server = http.createServer(options, requestListener());
 
-    if (!onListen) {
-        onListen = defaultListener(server);
-    }
-    server.listen(8080, onListen);
-
-    function defaultListener(server) {
-        return () => {
-            console.warn(`http-static running on ${  server.port}` );
-        };
-    }
-}
-//TODO lyfecicle managed using npm runscript
-startHttpd();
+server.listen(8080,()=> {
+  console.warn(`http-static running on UNKNOWN|8080 host an port.`);
+});
 
